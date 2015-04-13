e396857d9371a4a6528aa40ce034ac6c11896770
Ben Mann <ben@birminghamguitartuition.co.uk>
2015-04-13 23:26:22 +0100

    modified server env variable

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+if (typeof NODE_ENV === "undefined")
+	NODE_ENV="development";
+
+
 var mongoose = require('./config/mongoose'),
 	express = require('./config/express'),
 	passport = require('./config/passport');
