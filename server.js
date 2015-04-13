dc7a6f7275c7fffb4374b3c766113c543856aff3
Ben Mann <ben@birminghamguitartuition.co.uk>
2015-04-13 23:36:05 +0100

    back to normal env var

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-if (typeof NODE_ENV === "undefined")
-	NODE_ENV="development";
-
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var mongoose = require('./config/mongoose'),
 	express = require('./config/express'),
